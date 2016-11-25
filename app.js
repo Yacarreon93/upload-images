cc9a557bb58251dbe0ded8ae2c56cfe2af82f6a2
Yasser Carreón <yacasia@gmail.com>
2016-11-25 14:25:37 -0600

    Fixed "app.js" arrangement

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const express = require('express')
-const bodyParser = require('body-parser')
-const router = require('./routes')
-
 const app = express()
 
-app.use('/', router)
-
-// Set "JADE" as template engine
-app.set('view engine', 'jade')
-
+const bodyParser = require('body-parser')
 // Parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
 // Parse application/json 
 app.use(bodyParser.json())
+
+const router = require('./routes')
+// Route from root directory
+app.use('/', router)
+
+// Set "jade" as template engine
+app.set('view engine', 'jade')
 
 module.exports = app
