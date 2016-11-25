2cbe3f101513f8e1a901cf963f23d556a8b8a47b
Yasser Carreón <yacasia@gmail.com>
2016-11-25 11:24:16 -0600

    Added config file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const app = require('./app')
+const config = require('./config.js')
 
-app.listen(3000, () => {
+app.listen(config.port, () => {
 
-    console.log('Server runs on http://localhost:3000')
+    console.log(`Server runs on http://localhost:${config.port}`)
 
 })  
