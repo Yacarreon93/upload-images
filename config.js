5a25d0275bf530d82d50cfa84ca4909a9b9fa537
Yasser Carreón <yacasia@gmail.com>
2016-11-25 12:10:13 -0600

    Included config properties for database

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
-module.exports = {
-    port: process.env.PORT || 3000	
+ const config = {
+    PORT: process.env.PORT || 3000,
+    DB_HOST: 'localhost',
+    DB_PORT: '27017',
+    DB_NAME: 'images-test',    
 }
+
+config.DB = `mongodb://${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`
+
+module.exports = config
