5aee207bd360558b7697979420638334f53a1705
Yasser Carreón <yacasia@gmail.com>
2016-11-25 12:10:30 -0600

    Included connection to database

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict'
 
 const app = require('./app')
+const mongoose = require('mongoose')
 const config = require('./config.js')
 
-app.listen(config.port, () => {
+console.log(config.DB)
 
-    console.log(`Server runs on http://localhost:${config.port}`)
+mongoose.connect(config.DB, (err, res) => {
 
-})  
+    if (err) {
+        return console.log('ERROR: Unable to connect to database >', err)        
+    }
+
+    console.log('Connected to database successfully')
+
+    app.listen(config.PORT, () => {
+
+        console.log(`Server runs on http://localhost:${config.PORT}`)
+
+    })  
+
+})
