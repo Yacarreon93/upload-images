341fffa69f9cc55664ec87caf77005f1b62540ed
Yasser Carreón <yacasia@gmail.com>
2016-11-25 13:14:30 -0600

    Created image model

---
+++ models/image.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Image schema details
+const ImageSchema = Schema({
+
+	title: String,
+	description: String,
+	path: { type: String, default: '/img/dummy.png' }
+
+})
+
+// Export Image schema as a model
+module.exports = mongoose.model('Image', ImageSchema)
