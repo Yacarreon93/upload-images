6871e4b3c7510b238158ba289d7cf53f89ff9086
Yasser Carreón <yacasia@gmail.com>
2016-11-25 12:41:30 -0600

    Added some comments for routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const express = require('express')
-
 const router = express.Router()
 
+// Routes:
+
+// Display the upload image form
 router.get('/image', (req, res) => {
 
     // Render jade template for the index view
