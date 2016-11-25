62fd4b62b514613a620fbe929572e4aa72f5b72f
Yasser Carreón <yacasia@gmail.com>
2016-11-25 13:40:38 -0600

    Included "body-parser" into app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const router = require('./routes')
+
 const app = express()
 
-const router = require('./routes')
+app.use('/', router)
 
 // Set "JADE" as template engine
 app.set('view engine', 'jade')
-app.use('/', router)
+
+// Parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+// Parse application/json 
+app.use(bodyParser.json())
 
 module.exports = app
