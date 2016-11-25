4efe3edc77bc316a2ea5e3b7bf6755670f21338a
Yasser Carreón <yacasia@gmail.com>
2016-11-25 14:26:03 -0600

    Tested image save method

--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,24 @@
 'use strict'
 
+// Import Image model
 const Image = require('../models/image')
 
+// Save image
 function save(req, res) {
 
-    console.log(req)
-    res.status(200).send('Saving image from controller...')
+    console.log('Saving image from controller...')
+
+    // Create a new image from model
+    let image = new Image()
+    
+    // Asign requested data
+    image.title = req.body.title
+	image.description = req.body.description
+
+    // Testing
+    res.status(200).send(image)
 
 }
 
 module.exports.save = save;
+	
