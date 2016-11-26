ae0ec97de357fd2e4991d28269813c2c03ad8ec6
Yasser Carreón <yacasia@gmail.com>
2016-11-25 18:00:22 -0600

    Included some comments to "index.js"

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require('./app')
 const mongoose = require('mongoose')
 const config = require('./config.js')
 
+// Connection to monogoDB can be setup from config.js
 mongoose.connect(config.DB, (err, res) => {
 
     if (err) {
@@ -12,6 +13,7 @@ mongoose.connect(config.DB, (err, res) => {
 
     console.log('Connected to database successfully')
 
+    // Web server can be setup from config.js
     app.listen(config.PORT, () => {
 
         console.log(`Server runs on http://localhost:${config.PORT}`)
